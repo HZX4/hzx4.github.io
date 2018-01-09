6758a55b0996344a936dba9bf2dcd6cd2736eaf2
HZX4 <33665348+HZX4@users.noreply.github.com>
2018-01-09 11:05:51 -0500

    Cleaner code
    More efficient use of classes and more efficient initialization scripts

--- initScripts.js
+++ initScripts.js
@@ -1,27 +1,14 @@
 $(function(){
-  var topm = (100-$("#name").height())/2;
-  $("#name").css("marginTop",topm);
-  var topu= (100-$("#nav-contain ul").height())/2;
-  $("#nav-contain ul").css("marginTop",topu);
-  var h = $(".sblock").first().width();
-  $(".sblock").height(h);
-  $(".lblock").height(h*2);
-  sw = $(".sqpr").width();
-  $(".sqpr").height(sw);
-  $(".sqpr img").width(sw).height(sw);
   $("#greeter-image").height($("#greeter-image").parent().height());
-  gi = ($("#greeter-image").height()-$("#greeter-image img").height())/2;
-  $("#greeter-image img").css("marginTop", gi);
   $("#splash").height($(window).height()-100);
-  var y = ($("#splash-text").parent().height()-$("#splash-text").height())/2;
-  //console.log(y);
-  $("#splash-text").css("margin-top", y);
 
+  //unslider scripts
   $('.my-slider').unslider({infinite:true});
   $(".unslider-arrow").each(function(){
     $(this).css("top", ($(this).parent().height()-$(this).height())/2);
   })
 
+  //centering scripts
   $(".centermeV").each(function(){
     $(this).css("marginTop", ($(this).parent().height()-$(this).height())/2);
   });
